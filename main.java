f9f33b2c4ecd0df1bb0ac4dca76b2367f90cfd14
Matias Figueroa <mathyaz77@gmail.com>
2022-10-03 19:01:28 -0300

    Creacion de la clase main e input con la cantidad de dinero a convertir

---
+++ main.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+public class main {
+
+	public static void main(String[] args) {
+			double plata;
+			plata=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de dinero que deseas convertir: "));
+			
+			JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+	}
+
+}
