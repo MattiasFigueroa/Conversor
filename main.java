d38f029c60e48cd3d5e017c480a4a6a270a6f39e
Matias Figueroa <mathyaz77@gmail.com>
2022-10-06 12:54:01 -0300

    Creacion de Switch con opciones para convertir la Moneda

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 			
 			
-			//to do menu para que el usuario seleccione el tipo de conversion
+			//Creacion de array para guardar los tipos de cambio que puede seleccionar el usuario
 			String[] opciones= {
 					"De Pesos a Dolares",
 					"De Pesos a Euros",
@@ -14,13 +14,10 @@ public class main {
 					"De Pesos a Wones",
 			};
 		
-			//Despliega menu de opciones con tipos de moneda
+			//Despliega menu de opciones con tipos de Cambios
 			//(String)JOptionPane.showInputDialog(Componente padre, Mensaje, Titulo, Tipo De Mensaje, Icono, Arreglo de opciones, Seleccion por defecto );
 			String tipoMoneda=(String)JOptionPane.showInputDialog(null,"Elija la moneda a la que deseas convertir tu dinero:","MONEDAS",JOptionPane.DEFAULT_OPTION,null,opciones,opciones[0]);
 			
-			//System.out.println(opciones[4]);
-			//System.out.println(tipoMoneda);
-		
 			
 			//recibe la cantidad de pesos a convertir
 			double plata;
@@ -29,15 +26,73 @@ public class main {
 			//to do validacion del input (que solo admita numeros)
 			
 			
-			Moneda dolar=new Yen ();
+			//Segun el tipo de moneda que selecciono el usuario, se proseguira de acuerdo al caso
+			switch (tipoMoneda) {
+				case "De Pesos a Dolares":
+					
+					Moneda cambioDolar=new Dolar();
+					
+					cambioDolar.setCantidadConvertir(plata);
+					//JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+					
+					double converti=cambioDolar.convertirMonedaa();
+					JOptionPane.showMessageDialog(null,"Lo que te corresponde en dolares es: "+converti);
+					
+					break;
+				
+				case "De Pesos a Euros":
+					
+					Moneda cambioEuro=new Euro();
+					
+					cambioEuro.setCantidadConvertir(plata);
+					//JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+					
+					double converti2=cambioEuro.convertirMonedaa();
+					JOptionPane.showMessageDialog(null,"Lo que te corresponde en Euros es: "+converti2);
+					break;	
+				
+				case "De Pesos a Libras Esterlinas":
+					
+					Moneda cambioLibra=new Libra();
+					
+					cambioLibra.setCantidadConvertir(plata);
+					//JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+					
+					double converti3=cambioLibra.convertirMonedaa();
+					JOptionPane.showMessageDialog(null,"Lo que te corresponde en Libras Esterlinas es: "+converti3);
+					break;
+				
+				case "De Pesos a Yenes":
+					
+					Moneda cambioYen=new Yen();
+					
+					cambioYen.setCantidadConvertir(plata);
+					//JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+					
+					double converti4=cambioYen.convertirMonedaa();
+					JOptionPane.showMessageDialog(null,"Lo que te corresponde en Yenes es: "+converti4);
+					break;
+				
+				case "De Pesos a Wones":
+					
+					Moneda cambioWon=new Won();
+					
+					cambioWon.setCantidadConvertir(plata);
+					//JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
+					
+					double converti5=cambioWon.convertirMonedaa();
+					JOptionPane.showMessageDialog(null,"Lo que te corresponde en Wones es: "+converti5);
+					break;
+					
+				default:
+					break;
+			}
+			
+
 			
 			
-			
-			dolar.setCantidadConvertir(plata);
-			JOptionPane.showMessageDialog(null, "el numero ingresado es: "+plata);
-			
-			double converti=dolar.convertirMonedaa();
-			JOptionPane.showMessageDialog(null,"Lo que te corresponde en dolares es: "+converti);
+					
+				
 			
 	}
 	
